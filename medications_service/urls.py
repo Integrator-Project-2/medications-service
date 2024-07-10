e370dda4e60f8f12be99c5f93adc4505f1493480
KarlaJulyana <karla@solarz.com.br>
2024-07-10 10:01:31 -0300

    chore: add api to app urls in project/urls.py

--- medications_service/urls.py
+++ medications_service/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('medications.urls')),
-    path('', include('reminders.urls')),
+    path('api/', include('medications.urls')),
+    path('api/', include('reminders.urls')),
 
 ]
