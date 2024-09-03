8b85e1ec26b5aa0705e15eac08a52bec05dcc983
Valéria <vvqf2016@gmail.com>
2024-09-03 16:49:32 -0300

    chore: altered search query params

--- medications/views.py
+++ medications/views.py
@@ -1,19 +1,16 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Medication
 from .serializers import MedicationSerializer
-from rest_framework.decorators import action
-from rest_framework.response import Response
 
 class MedicationViewSet(viewsets.ModelViewSet):
     queryset = Medication.objects.all()
     serializer_class = MedicationSerializer
 
-    @action(detail=False, methods=['get'])
-    def search(self, request):
-        name = request.query_params.get('name', None)
-        if name is not None:
-            medications = self.queryset.filter(name__icontains=name)
-        else:
-            medications = self.queryset.all()
-        serializer = self.get_serializer(medications, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.query_params.get('name', None)
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
